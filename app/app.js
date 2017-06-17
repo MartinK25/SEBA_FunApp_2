51e445db00e5541c541dbf8e6c3a4b8ded1f6f0b
Martin Kiener <martin.kiener@gmx.de>
2017-06-17 21:26:24 +0200

    Edit login and startpage, still not functional!

--- app/app.js
+++ app/app.js
@@ -6,7 +6,8 @@
     'use strict';
 
     angular.module('app', [
-        "ui.router"
+        "ui.router",
+        "ngCookies"
     ])
         .config(function($stateProvider, $urlRouterProvider) {
             $urlRouterProvider.otherwise("/");
@@ -27,6 +28,11 @@
                 url: "/activities",
                 templateUrl: "./views/user/activity.html",
                 controller: "activityController"
+            }).state("login", {
+                url: "/login",
+                templateUrl: "./views/login/login.view.html",
+                controller: "LoginController",
+                controllerAs: 'vm'
             }).state("edit", {
                 url: "/edit/:id",
                 templateUrl: "./views/user/create.html",
@@ -40,4 +46,21 @@
         .constant("globalConfig", {
             apiAddress: "http://localhost:3000/api"
         });
+    run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
+    function run($rootScope, $location, $cookies, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookies.getObject('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
+            }
+        });
+    }
 })();
